836c552092662ee067766ca9bacabe2d0ad1cac2
lunarKettle <1evgeny.nesterenko1@gmail.com>
2025-03-20 23:06:00 +0300

    added README.md

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/lunarKettle/message-broker/internal/broker"
 	"github.com/lunarKettle/message-broker/internal/network"
+	"log/slog"
 	"os"
 )
 
@@ -15,6 +16,7 @@ func main() {
 	messageBroker := broker.NewMessageBroker()
 	tcpServer := network.NewTCPServer(serverAddress, messageBroker)
 
+	slog.Info("Starting server", "Address", serverAddress)
 	if err := tcpServer.Listen(); err != nil {
 		panic(err)
 	}
